9e36ea946e820fbedc665a3f2c2ee0d4bfa38c07
Mahrud Sayrafi <mahrud@berkeley.edu>
2018-01-26 19:21:56 -0800

    Added sample proofs and STR's

--- make_str.sh
+++ make_str.sh
@@ -1,1 +1,4 @@
 # str=sign_SK_d(t:t_prev:root_t:H(STR_pref):policy)
+echo "2018:2017:281901eba888ee630110d999bc84e2a42df364d8:H(null):default" | \
+  gpg2 --armor --clear-sign --output tree-2018.signed \
+       --local-user mahrud@berkeley.edu
